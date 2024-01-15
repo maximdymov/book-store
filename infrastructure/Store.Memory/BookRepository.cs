7e077ab980277fcbfcc4dd41d7f0c45a93890016
Maxim Dymov <soleft@mail.ru>
2024-01-15 15:33:32 +0300

    Добавлена реализация репозитория книг в памяти

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1, "Art Of Programming"),
+            new Book(2, "Refactoring"),
+            new Book(3, "C Programming Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            var b = new Book(1, "gfdg");
+            return books.Where(book => book.Title.Contains(titlePart))
+                        .ToArray();
+        }
+    }
+}
